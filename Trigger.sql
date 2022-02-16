28b11fdb4b989ea58b90713776f6aa2333531ad4
ka lung chan <klchan468-c@my.cityu.edu.hk>
2022-02-17 01:27:09 +0800

    Update Trigger.sql

--- Trigger.sql
+++ Trigger.sql
@@ -114,3 +114,101 @@ END;
 |
 DELIMITER ;
 
+DELIMITER |
+
+CREATE TRIGGER IF NOT EXISTS DetailInsertTrigger BEFORE INSERT ON `Detail`
+FOR EACH ROW
+BEGIN
+    DECLARE original_query text;
+    DECLARE orig_sourceIP VARCHAR(255);
+    DECLARE orig_user VARCHAR(255);
+    SET original_query = (SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_sourceIP = (SELECT `host` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_user = (SELECT `user` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    
+    INSERT INTO `auditlog`(`query`,TIMESTAMP,sourceIP,sourceAccount) VALUES (original_query,NOW(),orig_sourceIP,orig_user);
+END;
+|
+DELIMITER ;
+
+DELIMITER |
+
+CREATE TRIGGER IF NOT EXISTS DetailUpdateTrigger BEFORE UPDATE ON `Detail`
+FOR EACH ROW
+BEGIN
+    DECLARE original_query text;
+    DECLARE orig_sourceIP VARCHAR(255);
+    DECLARE orig_user VARCHAR(255);
+    SET original_query = (SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_sourceIP = (SELECT `host` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_user = (SELECT `user` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());    
+    INSERT INTO `auditlog`(`query`,TIMESTAMP,sourceIP,sourceAccount) VALUES (original_query,NOW(),orig_sourceIP,orig_user);
+END;
+|
+DELIMITER ;
+
+DELIMITER |
+
+CREATE TRIGGER IF NOT EXISTS AttachmentInsertTrigger BEFORE INSERT ON `Attachment`
+FOR EACH ROW
+BEGIN
+    DECLARE original_query text;
+    DECLARE orig_sourceIP VARCHAR(255);
+    DECLARE orig_user VARCHAR(255);
+    SET original_query = (SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_sourceIP = (SELECT `host` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_user = (SELECT `user` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    
+    INSERT INTO `auditlog`(`query`,TIMESTAMP,sourceIP,sourceAccount) VALUES (original_query,NOW(),orig_sourceIP,orig_user);
+END;
+|
+DELIMITER ;
+
+DELIMITER |
+
+CREATE TRIGGER IF NOT EXISTS AttachmentUpdateTrigger BEFORE UPDATE ON `Attachment`
+FOR EACH ROW
+BEGIN
+    DECLARE original_query text;
+    DECLARE orig_sourceIP VARCHAR(255);
+    DECLARE orig_user VARCHAR(255);
+    SET original_query = (SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_sourceIP = (SELECT `host` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_user = (SELECT `user` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());    
+    INSERT INTO `auditlog`(`query`,TIMESTAMP,sourceIP,sourceAccount) VALUES (original_query,NOW(),orig_sourceIP,orig_user);
+END;
+|
+DELIMITER ;
+
+DELIMITER |
+
+CREATE TRIGGER IF NOT EXISTS TagInsertTrigger BEFORE INSERT ON `Tag`
+FOR EACH ROW
+BEGIN
+    DECLARE original_query text;
+    DECLARE orig_sourceIP VARCHAR(255);
+    DECLARE orig_user VARCHAR(255);
+    SET original_query = (SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_sourceIP = (SELECT `host` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_user = (SELECT `user` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    
+    INSERT INTO `auditlog`(`query`,TIMESTAMP,sourceIP,sourceAccount) VALUES (original_query,NOW(),orig_sourceIP,orig_user);
+END;
+|
+DELIMITER ;
+
+DELIMITER |
+
+CREATE TRIGGER IF NOT EXISTS TagUpdateTrigger BEFORE DELETE ON `Tag`
+FOR EACH ROW
+BEGIN
+    DECLARE original_query text;
+    DECLARE orig_sourceIP VARCHAR(255);
+    DECLARE orig_user VARCHAR(255);
+    SET original_query = (SELECT info FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_sourceIP = (SELECT `host` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());
+    SET orig_user = (SELECT `user` FROM INFORMATION_SCHEMA.PROCESSLIST WHERE id = CONNECTION_ID());    
+    INSERT INTO `auditlog`(`query`,TIMESTAMP,sourceIP,sourceAccount) VALUES (original_query,NOW(),orig_sourceIP,orig_user);
+END;
+|
+DELIMITER ;
